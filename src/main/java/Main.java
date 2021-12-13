0aad1f7d86c44d17c3545c6ce9ea63cdde4f0e60
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-12-13 12:53:07 -0500

    able to print number of milks, number inaccurate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,36 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main {
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
-        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+        String starter = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+
+        String result = checkForMilk(starter);
+//        result = result + checkForBread(starter);
+//        result = result + checkForCookies(starter);
+//        result = result + checkForApples(starter);
+
+
+        return result;
+    }
+
+    private String checkForMilk(String starter) {
+        String text = starter;
+        String patternString = "Milk";
+        Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+        int occurances = 0;
+        while(matcher.find()){
+            occurances++;
+        }
+        String result = "name:    Milk      seen:  " + occurances + " times\n"
+                        + "=============      ==============\n";
+
+
         return result;
     }
 
