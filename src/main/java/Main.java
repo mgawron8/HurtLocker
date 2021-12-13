b88ad083e40b6aaed6f78ea375f444f28a86030f
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-12-13 13:47:59 -0500

    previous way was too forced, trying to find a more adaptable way

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,30 +9,47 @@ public class Main {
         ClassLoader classLoader = getClass().getClassLoader();
         String starter = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
 
-        String result = checkForMilk(starter);
+//        String result = checkForMilk(starter);
 //        result = result + checkForBread(starter);
 //        result = result + checkForCookies(starter);
 //        result = result + checkForApples(starter);
 
 
-        return result;
+//        return result;
+        return starter;
     }
 
-    private String checkForMilk(String starter) {
+//    private String checkForApples(String starter) {
+//        return checkFor(starter, "Apples");
+//    }
+//
+//    private String checkForCookies(String starter) {
+//        return checkFor(starter, "Cookies");
+//    }
+//
+//    private String checkForBread(String starter) {
+//        return checkFor(starter, "Bread");
+//    }
+//
+//    private String checkForMilk(String starter) {
+//        return checkFor(starter, "Milk");
+//    }
+
+    private String checkFor(String starter, String key){
         String text = starter;
-        String patternString = "Milk";
+        String patternString = "##(.*)##";
         Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(text);
         int occurances = 0;
         while(matcher.find()){
             occurances++;
         }
-        String result = "name:    Milk      seen:  " + occurances + " times\n"
-                        + "=============      ==============\n";
-
+        String result = "name:    " + patternString + "      seen:  " + occurances + " times\n"
+                + "=============      ==============\n";
 
         return result;
     }
+
 
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
